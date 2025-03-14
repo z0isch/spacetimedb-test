0530c3906c5af45cb7ef791192be6f6ae06eccf1
AJ Ruf <z0isch@gmail.com>
2025-03-14 12:34:22 -0400

    Try ./

--- docs/js/sound-events.js
+++ docs/js/sound-events.js
@@ -1,11 +1,13 @@
 (function () {
   const eventSoundDict = {
-    WrongGuess: new Audio("../sounds/Errors and Cancel/Cancel 1.m4a"),
-    CorrectGuess: new Audio("../sounds/Complete and Success/Success 2.m4a"),
-    MyTurn: new Audio("../sounds/Notifications and Alerts/Notification 3.m4a"),
-    TimeUp: new Audio("../sounds/Errors and Cancel/Error 5.m4a"),
-    IWin: new Audio("../sounds/Notifications and Alerts/Notification 9.m4a"),
-    ILose: new Audio("../sounds/Errors and Cancel/Error 4.m4a"),
+    WrongGuess: new Audio("./../sounds/Errors and Cancel/Cancel 1.m4a"),
+    CorrectGuess: new Audio("./../sounds/Complete and Success/Success 2.m4a"),
+    MyTurn: new Audio(
+      "./../sounds/Notifications and Alerts/Notification 3.m4a"
+    ),
+    TimeUp: new Audio("./../sounds/Errors and Cancel/Error 5.m4a"),
+    IWin: new Audio("./../sounds/Notifications and Alerts/Notification 9.m4a"),
+    ILose: new Audio("./../sounds/Errors and Cancel/Error 4.m4a"),
   };
   for (const event in eventSoundDict) {
     window.addEventListener(event, () => {
